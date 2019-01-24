6e0311915cdf327db12e61a08107325a30440ac0
buncis <buncismamen@gmail.com>
2019-01-24 20:09:46 +0700

    udah bisa query data dari cache

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,53 +2,74 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import { AUTH_TOKEN } from "../constants";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const NETWORK_STATUS = gql`
+  query NetworkStatus {
+    networkStatus {
+      isConnected
+    }
+  }
+`;
 
 class Header extends Component {
   render() {
     const authToken = localStorage.getItem(AUTH_TOKEN);
 
     return (
-      <div className="flex pa1 justify-between nowrap orange">
-        <div className="flex flex-fixed black">
-          <div className="fw7 mr1">Hacker News</div>
-          <Link to="/" className="ml1 no-underline black">
-            new
-          </Link>
-          <div className="ml1">|</div>
-          <Link to="/top" className="ml1 no-underline black">
-            top
-          </Link>
-          <div className="ml1">|</div>
-          <Link to="/search" className="ml1 no-underline black">
-            search
-          </Link>
-          {authToken && (
-            <div className="flex">
-              <div className="ml1">|</div>
-              <Link to="/create" className="ml1 no-underline black">
-                submit
-              </Link>
+      <Query query={NETWORK_STATUS}>
+        {({ data }) => {
+          const { isConnected } = data.networkStatus;
+          return (
+            <div className="flex pa1 justify-between nowrap orange">
+              <div className="flex flex-fixed black">
+                <div className="fw7 mr1">Hacker News</div>
+                <Link to="/" className="ml1 no-underline black">
+                  new
+                </Link>
+                <div className="ml1">|</div>
+                <Link to="/top" className="ml1 no-underline black">
+                  top
+                </Link>
+                <div className="ml1">|</div>
+                <Link to="/search" className="ml1 no-underline black">
+                  search
+                </Link>
+                {authToken && (
+                  <div className="flex">
+                    <div className="ml1">|</div>
+                    <Link to="/create" className="ml1 no-underline black">
+                      submit
+                    </Link>
+                  </div>
+                )}
+                <div className="ml1">|</div>
+                <div className="ml1 no-underline black">
+                  {isConnected ? "Online Mode" : "Offline Mode"}
+                </div>
+              </div>
+              <div className="flex flex-fixed">
+                {authToken ? (
+                  <div
+                    className="ml1 pointer black"
+                    onClick={() => {
+                      localStorage.removeItem(AUTH_TOKEN);
+                      this.props.history.push(`/`);
+                    }}
+                  >
+                    logout
+                  </div>
+                ) : (
+                  <Link to="/login" className="ml1 no-underline black">
+                    login
+                  </Link>
+                )}
+              </div>
             </div>
-          )}
-        </div>
-        <div className="flex flex-fixed">
-          {authToken ? (
-            <div
-              className="ml1 pointer black"
-              onClick={() => {
-                localStorage.removeItem(AUTH_TOKEN);
-                this.props.history.push(`/`);
-              }}
-            >
-              logout
-            </div>
-          ) : (
-            <Link to="/login" className="ml1 no-underline black">
-              login
-            </Link>
-          )}
-        </div>
-      </div>
+          );
+        }}
+      </Query>
     );
   }
 }
